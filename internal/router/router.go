7ab8e932ec5d0cde77c0e0c036927b413831ff33
Marcus Xavier <marcusxavierr123@gmail.com>
2024-01-28 00:33:16 -0300

    feat: creating basic configuration for chi

---
+++ internal/router/router.go
@@ -0,0 +1,32 @@
+package router
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func Initialize() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	// Just an Hello world route
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello friend\n"))
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	address := "0.0.0.0:" + port
+	fmt.Println("Running on port", address)
+
+	if err := http.ListenAndServe(address, router); err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/MarcusXavierr/search-engine-api/internal/router"
+)
 
 func main() {
-	fmt.Println("Hello friend")
+	router.Initialize()
 }
